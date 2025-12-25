ad7b6e4fa17b45f1146a453b3fafdd03da81dd5f
GERRIT VISSER <gervisz@live.nl>
2025-12-25 22:54:50 +0100

    Add loading animation

--- project-04-photo-gallery/src/app/page.js
+++ project-04-photo-gallery/src/app/page.js
@@ -13,11 +13,12 @@ export default function Home() {
         const res = await fetch("/api/nasa");
         const data = await res.json();
         setImage(data); // image is an object
-        console.log(data);
       } catch (err) {
         console.error(err);
       } finally {
-        setLoading(false);
+        setTimeout(() => {
+          setLoading(false);
+        }, 3000);
       }
     };
 
@@ -27,7 +28,13 @@ export default function Home() {
   if (loading) {
     return (
       <main>
-        <p>Loading...</p>
+        <div className="loader">
+          <div className="loader__moon"></div>
+          <div className="loader__orbit-container">
+            <div className="loader__satellite"></div>
+          </div>
+        </div>
+        <p className="loader__text">Fetching data from space…</p>
       </main>
     );
   }
@@ -47,30 +54,39 @@ export default function Home() {
 
   return (
     <main>
-      <Image
-        src={image.url}
-        alt={image.title}
-        width={750}
-        height={500}
-        sizes="100vw"
-        style={{ width: "100%", height: "auto" }}
-        priority
-      />
-      <h2 className="title">{image.title}</h2>
-      <p className="date">
-        {new Date(image.date).toLocaleDateString("en-US", {
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        })}
-      </p>
-      <p className="explanation">{expanded ? image.explanation : preview}</p>
+      <div className="hero">
+        <Image
+          className="hero__image"
+          src={image.url}
+          alt={image.title}
+          width={750}
+          height={500}
+          sizes="100vw"
+          style={{ width: "100%", height: "auto" }}
+          priority
+        />
+        <h3 className="hero__heading">{image.title}</h3>
+        <p className="hero__date">
+          {new Date(image.date).toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </p>
+        <p className="hero__explanation">
+          {expanded ? image.explanation : preview}
+        </p>
 
-      {isLong && (
-        <button onClick={() => setExpanded(!expanded)}>
-          {expanded ? "Show less ▲" : "Read more ▼"}
-        </button>
-      )}
+        {isLong && (
+          <button
+            className="hero__toggle"
+            onClick={() => setExpanded(!expanded)}
+          >
+            {expanded ? "Show less ▲" : "Read more ▼"}
+          </button>
+        )}
+      </div>
+      <div className="grid"></div>
     </main>
   );
 }
