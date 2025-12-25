5816da9e0da57e2b26d8d6572e39705e8698b74f
GERRIT VISSER <gervisz@live.nl>
2025-12-25 22:54:17 +0100

    Change classNames of headings

--- project-04-photo-gallery/src/app/components/Header.js
+++ project-04-photo-gallery/src/app/components/Header.js
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
     <header>
-      <h1>Nasa</h1>
-      <h2>Astronomy Picture of the Day</h2>
+      <h1 className="header__heading">Nasa</h1>
+      <h2 className="header__heading-sub">Astronomy Picture of the Day</h2>
     </header>
   );
 }
