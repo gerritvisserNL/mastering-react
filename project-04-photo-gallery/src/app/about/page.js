b5f1aecf6a0accd78e7145a1c86cb6567a2b695f
GERRIT VISSER <gervisz@live.nl>
2025-12-30 22:16:32 +0100

    Add state to Favorites and show favorites on page

--- project-04-photo-gallery/src/app/about/page.js
+++ project-04-photo-gallery/src/app/about/page.js
@@ -1,4 +1,4 @@
-export default function Page() {
+export default function About() {
   return (
     <main>
       <p>About page</p>
--- project-04-photo-gallery/src/app/favorites/page.js
+++ project-04-photo-gallery/src/app/favorites/page.js
@@ -1,7 +1,18 @@
-export default function Page() {
+"use client";
+import { useState } from "react";
+import PhotoCard from "../components/PhotoCard";
+
+export default function Favorites() {
+  const [photos] = useState(() => {
+    if (typeof window === "undefined") return [];
+    return JSON.parse(localStorage.getItem("favorites")) || [];
+  });
+
   return (
     <main>
-      <p>Favorites page</p>
+      {photos.map((photo) => (
+        <PhotoCard key={photo.date} photo={photo} />
+      ))}
     </main>
   );
 }
