93785521f2df101a29811ff84bc50763716a3b46
GERRIT VISSER <gervisz@live.nl>
2026-01-06 20:10:42 +0100

    Initial setup project 06 - photo gallery 2

---
+++ project-06-photo-gallery2/src/app/about/page.js
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <main>
+      <h2>About page</h2>
+    </main>
+  );
+}
---
+++ project-06-photo-gallery2/src/app/favorites/page.js
@@ -0,0 +1,7 @@
+export default function Favorites() {
+  return (
+    <main>
+      <h2>Favorites page</h2>
+    </main>
+  );
+}
