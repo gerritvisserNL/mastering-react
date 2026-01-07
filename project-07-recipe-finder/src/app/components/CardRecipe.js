86139680a086d5d9d44a479ae8c65ba428f9ebb7
GERRIT VISSER <gervisz@live.nl>
2026-01-07 16:35:33 +0100

    wip: Card layout

--- project-07-recipe-finder/src/app/components/CardRecipe.js
+++ project-07-recipe-finder/src/app/components/CardRecipe.js
@@ -3,15 +3,23 @@ import Image from "next/image";
 export default function CardRecipe({ recipe }) {
   return (
     <div className="card">
-      <Image
-        src={recipe.image}
-        alt={recipe.title}
-        className="card__image"
-        width={278}
-        height={185}
-      />
+      <div className="card__image-wrapper">
+        <Image
+          src={recipe.image}
+          alt={recipe.title}
+          className="card__image"
+          width={278}
+          height={185}
+        />
+      </div>
       <div className="card__content">
         <h3 className="card__heading">{recipe.title}</h3>
+        <p>⏱ {recipe.readyInMinutes} min</p>
+        <p>🍽 {recipe.servings} servings</p>
+        {recipe.vegetarian && <p>🥬 Vegetarian</p>}
+        {recipe.vegan && <p>🌱 Vegan</p>}
+        {recipe.glutenFree && <p> Gluten Free </p>}
+        {recipe.dairyFree && <p> Lactose Free </p>}
       </div>
     </div>
   );
