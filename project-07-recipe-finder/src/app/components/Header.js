d56dee793cb02605db203d2d10c4830edfbd5cfb
GERRIT VISSER <gervisz@live.nl>
2026-01-09 15:58:17 +0100

    Add navigation pathname and active-link to className links

--- project-07-recipe-finder/src/app/components/Header.js
+++ project-07-recipe-finder/src/app/components/Header.js
@@ -1,19 +1,35 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header>
       <h1>Recipe finder</h1>
       <nav>
         <ul>
           <li>
-            <Link href="/">Home</Link>
+            <Link href="/" className={pathname === "/" ? "active-link" : ""}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link href="/favorites">Favorites</Link>
+            <Link
+              href="/favorites"
+              className={pathname === "/favorites" ? "active-link" : ""}
+            >
+              Favorites
+            </Link>
           </li>
           <li>
-            <Link href="/custom">Custom</Link>
+            <Link
+              href="/custom"
+              className={pathname === "/custom" ? "active-link" : ""}
+            >
+              Custom
+            </Link>
           </li>
         </ul>
       </nav>
